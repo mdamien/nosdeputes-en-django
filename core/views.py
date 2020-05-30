bd7e050ee23a3dc75004805d50db05d50e2ea5b1
Damien <damien@dam.io>
2020-05-30 18:01:33 +0200

    ca devient pas mal

--- core/views.py
+++ core/views.py
@@ -4,15 +4,47 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.core.serializers import serialize
 
-from .models import Intervention
+from .models import Intervention, Tag, Tagging
+
+
+def jsonify(queryset):
+    objs = json.loads(serialize('json', queryset))
+    for obj in objs:
+        for k, v in obj['fields'].items():
+            obj[k] = v
+        obj['id'] = obj['pk']
+        del obj['pk']
+        del obj['fields']
+        del obj['model']
+    return objs
+
 
 def api_seance(request):
     seance_id = 103
-    loiid = 820
+    loi_id = 820
+
     interventions = Intervention.objects.filter(seance_id=seance_id)
-    interventions = json.loads(serialize('json', interventions))
-    interventions = [inter['fields'] for inter in interventions]
-    # section_id -> id_dossier_an -> texteloi.id !
+    taggings = Tagging.objects.filter(taggable_model="Intervention", taggable_id__in=interventions)
+    tags = Tag.objects.filter(id__in=taggings.values('tag_id'), triple_key='numero')
+
+    interventions = jsonify(interventions)
+    taggings = jsonify(taggings)
+    tags = jsonify(tags)
+
+    # add tag infos to taggings
+    for tagging in taggings:
+        tagging['tags'] = [tag for tag in tags if tagging['tag_id'] == tag['id']]
+
+    # add tagging infos to interventions
+    for intervention in interventions:
+        intervention['taggings'] = [
+            tagging for tagging in taggings
+            if intervention['id'] == tagging['taggable_id']
+        ]
+
+    # keep only intervention about this law
+    interventions = [intervention for intervention in interventions if intervention['taggings']]
+
     data = {'seance': interventions}
 
     return JsonResponse(data)
